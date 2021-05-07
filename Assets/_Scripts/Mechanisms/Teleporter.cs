d2d6933089f261f28039e08d1e09f01c742485b2
rhum <lucas.rioust@gmail.com>
2021-05-07 16:36:34 +0200

    Level3

---
+++ Assets/_Scripts/Mechanisms/Teleporter.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField] private Teleporter To;
+    [SerializeField] private float cooldown;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    private bool inCooldown = false;
+
+    private void OnDrawGizmos()
+    {
+        if (To)
+            Debug.DrawLine(transform.position, To.transform.position);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (inCooldown || To == null)
+            return;
+        BoomObject boom;
+        if (collision.transform.TryGetComponent(out boom))
+        {
+            boom.transform.position = To.transform.position;
+            StartCoroutine(CooldownCoroutine());
+            To.StartCoroutine(To.CooldownCoroutine());
+        }
+    }
+
+    IEnumerator CooldownCoroutine()
+    {
+        inCooldown = true;
+        spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, .25f);
+
+        yield return new WaitForSeconds(cooldown);
+
+        inCooldown = false;
+        spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 1f);
+
+    }
+}
